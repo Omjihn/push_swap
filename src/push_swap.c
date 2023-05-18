d18c2830f88c49779291677a02588875cd0d79fa
Gregoire Bricot <gbricot@c1r1p2.42perpignan.fr>
2023-05-18 14:47:35 +0200

    sort 2 numbers condition

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: gbricot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 15:26:08 by gbricot           #+#    #+#             */
-/*   Updated: 2023/05/17 17:16:33 by gbricot          ###   ########.fr       */
+/*   Updated: 2023/05/18 14:19:35 by gbricot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,12 @@ void	ft_push_swap(t_stack *a, t_stack *b)
 		ft_add_index(a);
 	else
 		return ;
+	if (a->list[0]->index == 2 && a->list[1]->index == 1)
+	{
+		ft_sa(a);
+		ft_printf("sa\n");
+		ft_free_all(a, b);
+	}
 	ft_split_stack(a, b);
 	ft_sort_stacks(a, b);
 	ft_free_all(a, b);
